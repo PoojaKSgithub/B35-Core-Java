740d379079b332da57efe26cbd8c667ba4e647dc
DELL <DELL@DESKTOP-JJ4UU0I>
2022-11-25 07:53:40 +0530

    first commit

---
+++ Assignment-1/src/com/tns/operator/BitwiseOperator.java
@@ -0,0 +1,12 @@
+package com.tns.operator;
+
+public class BitwiseOperator {
+
+	public static void main(String[] args) {
+		int a=8,b=1;
+		int res=a << b;
+		System.out.println(res);
+
+	}
+
+}
---
+++ Assignment-1/src/com/tns/operator/LogicalOperator.java
@@ -0,0 +1,13 @@
+package com.tns.operator;
+
+public class LogicalOperator {
+
+	public static void main(String[] args) {
+		int a=3,b=4;
+		boolean res=!(a<b
+				);
+		System.out.println(res);
+
+	}
+
+}
