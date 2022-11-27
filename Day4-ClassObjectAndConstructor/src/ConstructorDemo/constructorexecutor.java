bed29449105ac9a8d0830febf447463813b99367
DELL <DELL@DESKTOP-JJ4UU0I>
2022-11-27 21:35:21 +0530

    FirstCommit

---
+++ Day4-ClassObjectAndConstructor/src/ConstructorDemo/constructorexecutor.java
@@ -0,0 +1,17 @@
+package ConstructorDemo;
+
+public class constructorexecutor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//object creation
+				Employee e=new Employee();
+				Employee e1=new Employee(60000,"Girisha doddamani");
+				Employee e2=new Employee(35000,"sudeep");
+
+				e1.print();
+				e2.print();
+              
+	}
+
+}
