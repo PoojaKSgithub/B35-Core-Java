ceaaf6ca79909c6833bd80a865477ca41c5abcd3
DELL <DELL@DESKTOP-JJ4UU0I>
2022-12-10 00:14:13 +0530

    firstcommit

---
+++ Day-9this_AndStaticKeyword/src/staticdemo/StaticKeywordDemo.java
@@ -0,0 +1,21 @@
+package staticdemo;
+
+public class StaticKeywordDemo {
+	//static variable
+		static int a;
+		float b;
+		
+		//static method
+		public static  void accept()
+		{
+			System.out.println("Pooja K S");
+		}
+		//static block is used to initialize static data members
+		static
+		{
+			a=26;
+			//we can't use non static variable inside static block
+			//b=89;
+		}
+
+}
