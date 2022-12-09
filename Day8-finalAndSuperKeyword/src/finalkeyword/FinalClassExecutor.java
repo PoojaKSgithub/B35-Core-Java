6d3dfb621dd7bf08a07a474290cde98837c57fc0
DELL <DELL@DESKTOP-JJ4UU0I>
2022-12-09 08:23:13 +0530

    firstcommit

---
+++ Day8-finalAndSuperKeyword/src/finalkeyword/FinalClassExecutor.java
@@ -0,0 +1,12 @@
+package finalkeyword;
+
+//driver class
+public class FinalClassExecutor {
+
+	public static void main(String[] args) {
+		finalchildclass f=new finalchildclass();
+		f.print();
+
+	}
+
+}
---
+++ Day8-finalAndSuperKeyword/src/finalkeyword/finalchildclass.java
@@ -0,0 +1,14 @@
+package finalkeyword;
+//child class
+public class finalchildclass extends FinalParentClass{
+	
+	//method
+	public void print()
+	{
+		System.out.println(name);
+	}
+	
+	
+	
+
+}
