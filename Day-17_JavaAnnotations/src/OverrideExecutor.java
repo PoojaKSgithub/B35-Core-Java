2d6a4f746e62066f5a01812ebd52914f96a99b35
DELL <DELL@DESKTOP-JJ4UU0I>
2022-12-19 15:09:07 +0530

    firstcommit

---
+++ Day-17_JavaAnnotations/src/OverrideExecutor.java
@@ -0,0 +1,13 @@
+
+
+//driver class
+public class OverrideExecutor {
+
+	public static void main(String[] args) {
+		Child c=new Child();
+		c.display();
+		
+
+	}
+
+}
---
+++ Day-17_JavaAnnotations/src/Parent.java
@@ -0,0 +1,14 @@
+
+//Super class
+public class Parent {
+	
+	public String name="Nita R";
+	public void display()
+	{
+		System.out.println("Parent Name: "+name);
+	}
+	
+	
+	
+
+}
